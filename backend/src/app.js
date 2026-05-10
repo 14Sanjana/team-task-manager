01144b6c8e33ba36be6b36fd192c657371e03057
Sanjana <sanjanagangadevi@gmail.com>
2026-05-10 14:39:09 +0530

    fixed import paths

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,33 @@
-const router = require('express').Router();
+const express = require('express');
 
-const {
-  getUsers
-} = require('../controllers/userController');
+const cors = require('cors');
 
-const authMiddleware = require('../middleware/authMiddleware');
+require('dotenv').config();
 
-router.get(
-  '/',
-  authMiddleware,
-  getUsers
-);
+const authRoutes = require('./routes/authRoutes');
 
-module.exports = router;
+const projectRoutes = require('./routes/projectRoutes');
+
+const taskRoutes = require('./routes/taskRoutes');
+
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
+app.use('/api/auth', authRoutes);
+
+app.use('/api/projects', projectRoutes);
+
+app.use('/api/tasks', taskRoutes);
+
+app.use('/api/users', userRoutes);
+
+module.exports = app;
